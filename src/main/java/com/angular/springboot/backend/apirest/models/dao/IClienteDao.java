9c36147b62fa4e560e099379ee3e0da646d4ce0b
Davids.berz <Davids.berz@gmail.com>
2020-01-12 13:08:32 -0600

    Agregando metodo con @Query

--- src/main/java/com/angular/springboot/backend/apirest/models/dao/IClienteDao.java
+++ src/main/java/com/angular/springboot/backend/apirest/models/dao/IClienteDao.java
@@ -2,5 +2,12 @@ package com.angular.springboot.backend.apirest.models.dao;
 
 import com.angular.backend.commons.models.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
-public interface IClienteDao extends JpaRepository<Cliente, Long> {}
+public interface IClienteDao extends JpaRepository<Cliente, Long> {
+
+    @Query("Select c From Cliente c")
+    List<Cliente> buscarTodos();
+
+}
